a8d0e05a8410cd6216cc94497234d323dce94260
Bryan Caballero <71765102+CBryan95@users.noreply.github.com>
2021-11-09 23:07:44 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -6,23 +6,25 @@ from classroom.asignatura import Asignatura
 
 class Grupo:
 
-    grado = None
+    grado = 12
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
+        self._asignaturas=[]
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
-        if(lista is None):
-            lista.append(alumno)
+    def agregarAlumno(self, alumno, lista=None):
+        if (lista is None) :
+            self.listadoAlumnos =[alumno]
+        else :
+            self.listadoAlumnos=[]
+            lista.append (alumno)
             self.listadoAlumnos = self.listadoAlumnos + lista
-        else:
-            self.listadoAlumnos = [alumno]
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
@@ -32,6 +34,9 @@ class Grupo:
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+    ##@ classmethod
+    ##def asignarNombre(cls, nombre="Grado 4"):
+       ## cls.grado = nombre
+
+    def __str__(self):
+        return("Grupo de estudiantes: " + self._grupo)
